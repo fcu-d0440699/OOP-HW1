67a842bef647124a88c1479d36aee79fa78a1742
Zhi <Zhi@shikozhi>
2016-10-04 21:38:35 +0800

    第一版程式碼

---
+++ src/fcu/iecs/oop/Hello.java
@@ -0,0 +1,15 @@
+package fcu.iecs.oop;
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		String lyrics = ("Let it go! Let it go ! Cannot hold anynore");
+		
+		lyrics = lyrics.replace( "it" , "her" );
+		lyrics = lyrics.replace( "Cannot" , "Can't" );
+		
+		System.out.println(lyrics);
+	}
+
+}
