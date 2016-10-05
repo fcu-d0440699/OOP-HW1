3e18362d6307b0aba51ab0d1b99777b3c7288c73
Zhi <Zhi@shikozhi>
2016-10-05 16:20:10 +0800

    第一版程式碼修改

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 
-		String lyrics = ("Let it go! Let it go ! Cannot hold anynore");
+		String lyrics = ("Let it go! Let it go ! Cannot hold it back anynore");
 		
 		lyrics = lyrics.replace( "it" , "her" );
 		lyrics = lyrics.replace( "Cannot" , "Can't" );
